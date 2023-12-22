6d68fb3bce526756b87a3053bf86abf7ea01bcdb
giovanatoch <tochtrop.giovana@gmail.com>
2023-12-22 19:32:23 -0300

    Ajusta o repsonsivo para celulares e tablets

--- js/script.js
+++ js/script.js
@@ -8,6 +8,15 @@ $('.navbar a').click(function (e){
     }, 1000)
 });
 
+$('.navbar-mobile a').click(function (e){
+    e.preventDefault();
+    var id = $(this).attr('href'),
+        targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - 30
+    }, 1000)
+});
+
 $(document).ready(function(){
     var cargos = ["Desenvolvedora Fron-End", "Analista de Suporte"];
     var atual = 0;
@@ -19,3 +28,20 @@ $(document).ready(function(){
         });
     }, 2000);
 });
+
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu');
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+})
+
+overlay.addEventListener('click', () => {
+    overlay.classList.remove('abrir-menu');
+})
